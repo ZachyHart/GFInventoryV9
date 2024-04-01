56b334d56a65bdb352f2b883bc9e2177ec0793fe
Johan Mari Ocampo <johanmari_ocampo@yahoo.com>
2024-04-01 19:45:47 +0800

    about us page

---
+++ AboutUs.php
@@ -0,0 +1,66 @@
+<?php
+require_once 'helpers/conn_helpers.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Got Funko Collections</title>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="sidebar-logo">
+                <img src="img/CircularLogo.jpg" alt="Logo" style="width: 100%; max-width: 120px; display: block; margin: 0 auto;">
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="CustomerInventory.php" class="sidebar-link" title="Our Products">
+                        <i class="lni lni-cart"></i>
+                        <span>Our Products</span>
+                    </a>
+                </li>
+                <!-- Second sidebar item for Feedback -->
+                <li class="sidebar-item">
+                    <a href="CustomerFeedback.php" class="sidebar-link" title="Feedback">
+                        <i class="lni lni-comments"></i>
+                        <span>Feedback</span>
+                    </a>
+                </li>
+                <!-- Add more sidebar items here if needed -->
+            </ul>
+            <div class="sidebar-footer">
+                <form action="controllers/Users.php" method="post" id="logout">
+                    <input type="hidden" name="type" value="logout">
+                    <a href="javascript:{}" onclick="document.getElementById('logout').submit();" class="sidebar-link" title="Logout">
+                        <i class="lni lni-exit"></i>
+                    </a>
+                </form>
+            </div>
+        </aside>
+
+        <!-- Main content area removed to focus only on sidebar -->
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="WorkingSidebar.js"></script>
+</body>
+
+</html>
